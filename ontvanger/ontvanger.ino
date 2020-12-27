9913c50ee03d60100070011d68d2a2ef974908fb
Pieter Coppens <pieter.nancy@gmail.com>
2020-12-27 23:14:56 +0100

    Rework to use NRF24L01 iso IR

--- ontvanger/ontvanger.ino
+++ ontvanger/ontvanger.ino
@@ -1,40 +1,50 @@
 #include <Adafruit_NeoPixel.h>
-#include <IRremote.h>
-
+//#include <IRremote.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 #define led_strip_pin   2
 #define AANTAL_LEDS 8
 
-int IR_ONTVANGER_PIN = 11;
+//int IR_ONTVANGER_PIN = 11;
+//IRrecv IRontvanger(IR_ONTVANGER_PIN);
+//decode_results IRResultaten;
+RF24 radio(7, 8);  // CE, CSN
+//11 (MOSI), 12(MISO) and 13(SCK) would be taken as well by the built-in NRF module
 
-IRrecv IRontvanger(IR_ONTVANGER_PIN);
+const byte address[6] = "00001";
 Adafruit_NeoPixel led_strip(AANTAL_LEDS, led_strip_pin, NEO_GRB + NEO_KHZ800);
-decode_results IRResultaten;
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);    
+    //pinMode(LED_BUILTIN, OUTPUT);    
     Serial.begin(115200);
     led_strip.begin();
-    IRontvanger.enableIRIn();  // Start de IR ontvanger
-    IRontvanger.blink13(true); // Feedback LED activeren
+    //IRontvanger.enableIRIn();  // Start de IR ontvanger
+    //IRontvanger.blink13(true); // Feedback LED activeren
+    while (!radio.begin()){
+      delay(100);
+      Serial.println("Radio niet geinitialiseerd");
+    }
+    Serial.println("Radio OK");
+    radio.setChannel(90); //2490 Mhz, buiten Wifi & medisch device spectrum
+    radio.setPALevel(RF24_PA_MAX);  //Max bereik
+    radio.setDataRate(RF24_1MBPS);  //Datarate = 1Mbps
+    radio.openReadingPipe(0, address);; //Adres instellen
+    radio.startListening(); //Zet als ontvanger
 }
 
 void loop() {
-    if (IRontvanger.decode(&IRResultaten)) {
-        Serial.println();
-        IRontvanger.printIRResultRaw(&Serial);
-        if (IRResultaten.decode_type == RC5){
-          Serial.println(IRResultaten.value, HEX);
-          byte rood =  map((IRResultaten.value & 0b0111110000000000)>>10, 0, 31, 0, 255); //kleurwaarde parsen & herschalen naar 0-255)
-          byte groen = map((IRResultaten.value &   0b0000001111100000)>>5, 0, 31, 0, 255);
-          byte blauw = map((IRResultaten.value &   0b0000000000011111)>>0, 0, 31, 0, 255);        
-          Serial.print("R: "); Serial.print(rood); Serial.print(" G: "); Serial.print(groen); Serial.print(" B: "); Serial.println(blauw);
-          led_strip.fill(led_strip.Color(rood, groen, blauw), 0, AANTAL_LEDS);
-          led_strip.show();
-          delay(5); // show() schakelt interrupts tijdelijk uit & en maakt daarmee IR ontvangst instabiel. Dus even wachten ;-)
-        }
-        else{
-          Serial.println("Incorrect IR-signaal");
-        }
-        IRontvanger.resume(); // Receive the next value
-    }
+  unsigned long kleur = 0;
+  if (radio.available())
+  {
+    Serial.println("Data ontvangen");    
+    radio.read(&kleur, sizeof(kleur));
+    Serial.println(kleur);
+    byte rood  = (kleur & 0b111111110000000000000000)>>16;
+    byte groen = (kleur & 0b000000001111111100000000)>>8;
+    byte blauw = (kleur & 0b000000000000000011111111)>>0;
+    Serial.print("R: "); Serial.print(rood); Serial.print(" G: "); Serial.print(groen); Serial.print(" B: "); Serial.println(blauw);
+    led_strip.fill(led_strip.Color(rood, groen, blauw), 0, AANTAL_LEDS);
+    led_strip.show();
+  }  
 }
--- zender/zender.ino
+++ zender/zender.ino
@@ -1,28 +1,51 @@
-#include <IRremote.h>
+//#include <IRremote.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
 #define r_weerstand_pin A0
 #define g_weerstand_pin A1
 #define b_weerstand_pin A2
 
-IRsend irsend;
-uint32_t vorige_kleur = 0;
+//IRsend irsend;
+RF24 radio(7, 8);  // CE, CSN
+const byte address[6] = "00001";
+unsigned long vorige_kleur = 0;
 
 void setup() {  
   Serial.begin(115200);
+  while (!radio.begin()){    
+    delay(100);
+    Serial.println("Radio niet geinitialiseerd");
+  }
+  Serial.println("Radio OK");
+  radio.setChannel(90); //2490 Mhz, buiten Wifi & medisch device spectrum
+  radio.setPALevel(RF24_PA_MAX);  //Max bereik
+  radio.setDataRate(RF24_1MBPS);  //Datarate = 1Mbps
+  radio.openWritingPipe(address); //Adres instellen
+  radio.stopListening(); //Zet als zender
 }
+
 void loop() {  
-  uint32_t kleur = 0;    
+  unsigned long kleur = 0;    
   int r_spanning = analogRead(r_weerstand_pin);
-  byte rood = map(r_spanning, 0, 1023, 0, 31); //range omzetten 0-1023 naar 0-31 (5bit, wegens 3 keuren als unsigned int (16b) versturen)
+  byte rood = map(r_spanning, 0, 1023, 0, 255); //range omzetten 0-1023 naar 0-255
+  //Serial.print("R: "); Serial.println(rood, HEX);
+  
   int g_spanning = analogRead(g_weerstand_pin);
-  byte groen = map(g_spanning, 0, 1023, 0, 31);
+  byte groen = map(g_spanning, 0, 1023, 0, 255);
+  //Serial.print("G: "); Serial.println(groen, HEX);
+  
   int b_spanning = analogRead(b_weerstand_pin);
-  byte blauw = map(b_spanning, 0, 1023, 0, 31);
-  kleur = (rood<<10) + (groen<<5) + (blauw); //3 kleuren in 1 unsigned int encoden (rood en groen x bits naar links shiften)    
+  byte blauw = map(b_spanning, 0, 1023, 0, 255);
+  //Serial.print("B: "); Serial.println(blauw, HEX);
+  
+  kleur = ((rood<<16) | (groen<<8) | (blauw)); //3 kleuren in 1 unsigned int encoden (rood en groen x bits naar links shiften)    
   if (kleur != vorige_kleur){ //Only send when value changes, otherwise batteries run out quickly
-    Serial.print("Kleur-code via IR doorsturen: "); Serial.println(kleur, HEX);
-	  irsend.sendRC5(kleur, 32);
+    Serial.print("Kleur-code doorsturen: "); Serial.println(kleur, HEX);
+	  //irsend.sendRC5(kleur, 32);
+    radio.write(&kleur, sizeof(kleur));
     vorige_kleur = kleur;
   }
-  delay(100);
+  delay(5);
 }
