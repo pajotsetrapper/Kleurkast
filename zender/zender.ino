511317811d67dbcb7fb3e1a79dda72e83ec8b4e3
Pieter Coppens <pieter.nancy@gmail.com>
2020-12-28 19:16:10 +0100

    Bugfix, use double for color components to allow bit shifting

--- zender/zender.ino
+++ zender/zender.ino
@@ -13,12 +13,12 @@ const byte address[6] = "00001";
 unsigned long vorige_kleur = 0;
 
 void setup() {  
-  Serial.begin(115200);
+  //Serial.begin(115200);
   while (!radio.begin()){    
     delay(100);
-    Serial.println("Radio niet geinitialiseerd");
+    //Serial.println("Radio niet geinitialiseerd");
   }
-  Serial.println("Radio OK");
+  //Serial.println("Radio OK");
   radio.setChannel(90); //2490 Mhz, buiten Wifi & medisch device spectrum
   radio.setPALevel(RF24_PA_MAX);  //Max bereik
   radio.setDataRate(RF24_1MBPS);  //Datarate = 1Mbps
@@ -27,23 +27,22 @@ void setup() {
 }
 
 void loop() {  
-  unsigned long kleur = 0;    
+  unsigned long kleur = 0;
   int r_spanning = analogRead(r_weerstand_pin);
-  byte rood = map(r_spanning, 0, 1023, 0, 255); //range omzetten 0-1023 naar 0-255
-  //Serial.print("R: "); Serial.println(rood, HEX);
+  unsigned long rood = map(r_spanning, 0, 1023, 0, 255); //range omzetten 0-1023 naar 0-255
   
   int g_spanning = analogRead(g_weerstand_pin);
-  byte groen = map(g_spanning, 0, 1023, 0, 255);
-  //Serial.print("G: "); Serial.println(groen, HEX);
+  unsigned long groen = map(g_spanning, 0, 1023, 0, 255);
   
   int b_spanning = analogRead(b_weerstand_pin);
-  byte blauw = map(b_spanning, 0, 1023, 0, 255);
-  //Serial.print("B: "); Serial.println(blauw, HEX);
+  unsigned long blauw = map(b_spanning, 0, 1023, 0, 255);
   
-  kleur = ((rood<<16) | (groen<<8) | (blauw)); //3 kleuren in 1 unsigned int encoden (rood en groen x bits naar links shiften)    
+  kleur = (rood<<16) + (groen<<8) + (blauw); //3 kleuren in 1 unsigned long encoden (rood en groen x bits naar links shiften)    
   if (kleur != vorige_kleur){ //Only send when value changes, otherwise batteries run out quickly
-    Serial.print("Kleur-code doorsturen: "); Serial.println(kleur, HEX);
-	  //irsend.sendRC5(kleur, 32);
+    //Serial.print("R: "); //Serial.println(rood, HEX);
+    //Serial.print("G: "); //Serial.println(groen, HEX);
+    //Serial.print("B: "); //Serial.println(blauw, HEX);
+    //Serial.print("Kleur-code doorsturen: "); //Serial.println(kleur, HEX);	  
     radio.write(&kleur, sizeof(kleur));
     vorige_kleur = kleur;
   }
